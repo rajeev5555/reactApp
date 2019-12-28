71b9df480d5f18081985019a8931503c74443e33
rajeev5555 <dubey.rajeev05rd@gmail.com>
2019-12-28 16:53:18 +0530

    New React App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import New from './New.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +19,10 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <New></New>
     </div>
+    
   );
 }
 
---
+++ src/New.js
@@ -0,0 +1,14 @@
+import React,{ Component } from 'react';
+
+
+class New extends Component{
+ render(){
+     return(
+         <div>
+             <p> Rajeev Dubey</p>
+         </div>
+     )
+ }
+}
+
+export default New;
